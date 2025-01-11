e22de993a8adbfadf32de6ec13ede767914036a9
Ukko Tägtström <ukko@tagt.fi>
2025-01-12 00:25:48 +0200

    L_11

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ def main():
     book_name = "The Frankenstein"
     book_path = "books/frankenstein.txt"
     file_contents = read_book(book_path)
-    # print(file_contents)           # Chapter 8
     word_count_result = word_count(file_contents)
-    # print(f"{book_name} contains {word_count_result} words") # Chapter 9
     char_count_result = char_count(file_contents)
-    print(char_count_result)
+    book_report(word_count_result, char_count_result)
 
 def read_book(book_path):
     with open(book_path) as f:
@@ -29,5 +27,16 @@ def char_count(file_contents):
 
     return char_count
 
+def book_report(word_count_result, char_count_result):
+    print(f"\t\tBegin Report\t\t\n")
+    
+    print(f"{word_count_result} words found in the document.\n")
+
+    sorted_char_count = sorted(char_count_result.items(), key=lambda x: x[1], reverse=True)
+
+    for char, count in sorted_char_count:
+        print(f"Character {char} was found {count} times.")
+    
+    print("\n\t\tEnd Report\t\t")
 
 main()
