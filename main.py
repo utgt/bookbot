145478cd0b2fb5ae083555cfaaa7e8bdc274719d
Ukko Tägtström <ukko@tagt.fi>
2025-01-12 00:11:13 +0200

    L_9

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 def main():
+    book_name = "The Frankenstein"
     book_path = "books/frankenstein.txt"
+    file_contents = read_book(book_path)
+    #print(file_contents)           # Chapter 8
+    word_count_result = word_count(file_contents)
+    print(f"{book_name} contains {word_count_result} words")
+
+
+def read_book(book_path):
     with open(book_path) as f:
-        file_contents = f.read()
-    print(file_contents)
+        return f.read()
+
+def word_count(file_contents):
+    count = file_contents.split()
+    return len(count)
 
 main()
