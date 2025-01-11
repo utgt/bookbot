b1e13e4d988564a296b165959a8d79e1388cda03
Ukko Tägtström <ukko@tagt.fi>
2025-01-12 00:25:48 +0200

    L_10

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ def main():
     book_name = "The Frankenstein"
     book_path = "books/frankenstein.txt"
     file_contents = read_book(book_path)
-    #print(file_contents)           # Chapter 8
+    # print(file_contents)           # Chapter 8
     word_count_result = word_count(file_contents)
-    print(f"{book_name} contains {word_count_result} words")
-
+    # print(f"{book_name} contains {word_count_result} words") # Chapter 9
+    char_count_result = char_count(file_contents)
+    print(char_count_result)
 
 def read_book(book_path):
     with open(book_path) as f:
@@ -14,5 +15,19 @@ def read_book(book_path):
 def word_count(file_contents):
     count = file_contents.split()
     return len(count)
+
+def char_count(file_contents):
+    file_contents = file_contents.lower()
+    char_count = {}
+
+    for char in file_contents:
+        if char.isalpha():
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+
+    return char_count
+
 
 main()
